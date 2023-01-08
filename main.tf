852d8b3c3878673796d0548a6e9b2626170bbbd1
imjoseangel <josea.munoz@gmail.com>
2023-01-08 20:59:56 +0100

    fix lint

--- main.tf
+++ main.tf
@@ -2,8 +2,8 @@
 # Local Declarations
 #---------------------------------------------------------
 locals {
-  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp.*.name, azurerm_resource_group.rg.*.name, [""]), 0)
-  location            = element(coalescelist(data.azurerm_resource_group.rgrp.*.location, azurerm_resource_group.rg.*.location, [""]), 0)
+  resource_group_name = element(coalescelist(data.azurerm_resource_group.rgrp[*].name, azurerm_resource_group.rg[*].name, [""]), 0)
+  location            = element(coalescelist(data.azurerm_resource_group.rgrp[*].location, azurerm_resource_group.rg[*].location, [""]), 0)
   roles_map           = { for role in var.roles : "${role.ppal_id}.${role.role}" => role }
 }
 
